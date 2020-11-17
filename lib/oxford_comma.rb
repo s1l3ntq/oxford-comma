7dabea9c5c4c16fe3782516476eea8ff9b2a1bfa
s1l3ntq <qimiller89@gmail.com>
2020-11-16 20:05:24 -0600

    commit tree ddf2335302a128e7bfa645cda4b5add6f82ff538

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length <= 2
+        return array.join(" and ")
+    else array.length >= 3
+        array[-1] = "and " + array[-1]
+        return array.join(", ")
+    end
 end
+
